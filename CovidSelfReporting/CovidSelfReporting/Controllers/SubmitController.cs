8dccecca63eaeea431bd753c18e1605d127aa849
Ciaran Finnegan <cfinnegan12@qub.ac.uk>
2020-12-09 17:21:47 +0000

    Frontend implementation

---
+++ CovidSelfReporting/CovidSelfReporting/Controllers/SubmitController.cs
@@ -0,0 +1,32 @@
+﻿using CovidSelfReporting.Data;
+using CovidSelfReporting.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+
+namespace CovidSelfReporting.Controllers
+{
+    public class SubmitController : Controller
+    {
+
+        private IReportService _reportService;
+        public SubmitController(IReportService reportService)
+        {
+            _reportService = reportService;
+        }
+
+        public IActionResult Index([FromQuery]string student_number, [FromQuery] string contact_number, [FromQuery] DateTime diagnosis_date)
+        {
+            Report report = new Report
+            {
+                StudentNumber = student_number,
+                Contact_Number = contact_number,
+                DiagnosisDate = diagnosis_date,
+                ReportDate = DateTime.Today
+            };
+
+            _reportService.AddReport(report);
+            return View();
+        }
+    }
+}
